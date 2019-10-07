bc92a6845b44b21dc1642bac64aecae32546a41a
baluadisys <53911022+baluadisys@users.noreply.github.com>
2019-10-07 09:12:18 +0530

    Bug fix

--- crosssitescripttest.js
+++ crosssitescripttest.js
@@ -3,28 +3,31 @@ $( document ).ready(function() {
     alert('cross site scripting');
     
     var classname = document.getElementsByClassName("glyphicon glyphicon-floppy-saved");
+	
+	var myFunction = function() {
+				var attribute = this.getAttribute("data-myattribute");
+				alert(attribute);
+				
+				var ele = document.getElementsByTagName('input');
 
-var myFunction = function() {
-    var attribute = this.getAttribute("data-myattribute");
-    alert(attribute);
-    var ele = document.getElementsByTagName('input');
+				// LOOP THROUGH EACH ELEMENT.
+				for (i = 0; i < ele.length; i++) {
 
-        // LOOP THROUGH EACH ELEMENT.
-        for (i = 0; i < ele.length; i++) {
+					// CHECK THE ELEMENT TYPE.
+					if (ele[i].type == 'text') {
+						console.log('Value: ' + ele[i].value);
 
-            // CHECK THE ELEMENT TYPE.
-            if (ele[i].type == 'text') {
-                console.log('Value: ' + ele[i].value);
+						for (j = 0; j < ele[i].attributes.length; j++) {
+							console.log(ele[i].attributes[j]);
+						}
+					}
 
-                for (j = 0; j < ele[i].attributes.length; j++) {
-                    console.log(ele[i].attributes[j]);
-                }
-            }
-    
-};
+				};
+	};
 
-for (var i = 0; i < classname.length; i++) {
-    classname[i].addEventListener('click', myFunction, false);
+	for (var i = 0; i < classname.length; i++) {
+		classname[i].addEventListener('click', myFunction, false);
+	}
+
+
 }
-    
-});
